9b3024ec0890a83d6fc950f4bf0e77ffb49c2713
blishchenko1981 <blishchenko1981@gmail.com>
2021-09-21 11:27:26 -0400

    day09 explicit wait

---
+++ src/test/java/com/cybertek/tests/day09_ExplicitWait/ExplicitWaitTest.java
@@ -0,0 +1,20 @@
+package com.cybertek.tests.day09_ExplicitWait;
+
+import com.cybertek.Utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class ExplicitWaitTest extends TestBase {
+    @Test
+
+    public void testWaitForTitle() {
+        //1. navigate to http://practice.cybertekschool.com/dynamic_loading
+
+        //2. Click on Example 7
+
+        //3. The title will show loading... until certain time ,
+        // we want to wait until the title value become "Dynamic Title"
+
+
+
+    }
+}
